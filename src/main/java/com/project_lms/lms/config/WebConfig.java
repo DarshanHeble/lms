eab960ff0f656ff38033f54b1b55811f12af85fb
Darshan Heble <darshanheble@gmail.com>
2025-04-24 12:13:50 +0530

    fixed a non null warning

--- src/main/java/com/project_lms/lms/config/WebConfig.java
+++ src/main/java/com/project_lms/lms/config/WebConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addFormatters(FormatterRegistry registry) {
+    public void addFormatters(@NonNull FormatterRegistry registry) {
         DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
         registrar.setUseIsoFormat(true);
         registrar.registerFormatters(registry);
